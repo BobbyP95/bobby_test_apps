d39285fb6125fbd8217cfa6645a7d04dc0470aa4
BobbyP95 <bobby.bp95@gmail.com>
2023-08-01 15:53:13 +0000

    Adding react router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
+import {Excuse} from './Excuse/Excuse'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
-
+const client = new QueryClient({
+  defaultOptions:{
+    queries:{
+      refetchOnWindowFocus:false,
+      refetchOnReconnect:false,
+    },
+  },
+});
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <QueryClientProvider client={client}>
+          <Excuse/>
+          <Router>
+          {/* <Navbar /> */}
+          <Routes>
+            <Route path="/" element={<Excuse />} />
+
+
+          </Routes>
+        </Router>
+        </QueryClientProvider>
+
+    </>
   );
 }
 
 export default App;
+
---
+++ src/Excuse/Excuse.js
@@ -0,0 +1,60 @@
+import React,{useState,useEffect} from 'react'
+import Axios from 'axios';
+import logo from '../logo.svg';
+import './excuse.css'
+import {useQuery} from '@tanstack/react-query'
+
+
+
+export const Excuse = () => {
+const [ type, setType ] = useState('gaming')
+// const [excuse,setExcuse] = useState(null)
+// useEffect(() =>{
+//     Axios.get(`https://excuser-three.vercel.app/v1/excuse/${type}/`).then((res)=>{
+//         setExcuse(res.data[0])
+//         console.log(res.data[0])
+//     })
+// },[type,])
+
+const {data,isError,isLoading,refetch} = useQuery(['01excuse'],()=>{
+    return Axios.get(`https://excuser-three.vercel.app/v1/excuse/${type}/`).then((res)=>res.data[0])
+})
+
+useEffect(()=>{
+    refetch()
+},[type,])
+
+if (isLoading){
+    return(  <div className="excuse-div error-div">
+        <h1 className='error'>Loading...</h1>
+    </div>)
+}
+if (isError){
+    return(  <div className=" excuse-div error-div">
+        <h1 className='error' >Sorry, there was an error</h1>
+    </div>)
+}
+
+  return (
+    <div className="App excuse-div">
+    <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div className=''>
+        <div >
+            <button onClick={()=>setType('gaming')}>Gaming</button>
+            <button onClick={()=>setType('developers')}>Developers</button>
+            <button onClick={()=>setType('office')}>Office</button>
+            <button onClick={()=>setType('funny')}>Funny </button>
+        </div>
+        
+        <div>
+            <h1>Category: {data?.category}</h1>
+            <h3>{data?.excuse}</h3>
+        </div>
+    </div>
+  
+    </header>
+
+  </div>
+  )
+}
